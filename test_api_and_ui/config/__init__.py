34b16aa61a65d736b3c5798377b8db803a5f6261
Ruslan91111 <ruslan_ru911@mail.ru>
2023-05-28 18:04:33 +0500

    new

---
+++ test_api_and_ui/config/__init__.py
@@ -0,0 +1,15 @@
+from test_api_and_ui.config.expectations import Expectations
+from test_api_and_ui.config.playwright import Playwright
+from test_api_and_ui.config.url import Url
+
+
+URL = Url()
+playwright = Playwright()
+expectations = Expectations()
+
+
+
+# print(URL)
+# print(playwright)
+# print(expectations)
+#
